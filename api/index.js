2aad5f0adc2307e1980bd77809a2a18a03f5cec4
Oscar Calderon <gregoriocalderon.07@hotmail.es>
2021-03-16 21:43:43 -0400

    documentacion swagger

--- api/index.js
+++ api/index.js
@@ -4,10 +4,14 @@ const config = require('../config');
 const user = require('./components/user/network');
 const app = express();
 
+const swaggerUi = require('swagger-ui-express');
+
 app.use(express.json());
+const swaggerDoc = require('./swagger.json');
 
 //router
 app.use('/api/user', user);
+app.use('./api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.listen(config.api.port, () => {
   console.log(`Api escuchando en el puerto ${config.api.port}`);
