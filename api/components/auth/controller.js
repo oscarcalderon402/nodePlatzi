76275d9093514b8b2833716b04972868bd5c5ac4
Oscar Calderon <gregoriocalderon.07@hotmail.es>
2021-03-23 02:38:58 -0400

    cifrar contrasena

--- api/components/auth/controller.js
+++ api/components/auth/controller.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const auth = require('../../../auth');
 const TABLA = 'auth';
 
@@ -9,16 +10,17 @@ module.exports = function (injectedStore) {
 
   async function login(username, password) {
     const data = await store.query(TABLA, { username: username });
-    if (data.password === password) {
+
+    const equals = await bcrypt.compare(password, data.password);
+    if (!equals) {
+      throw new Error('informacion invalida');
+    } else {
       //token
       return auth.sign(data);
-    } else {
-      throw new Error('informacion invalida');
     }
-    return data;
   }
 
-  function upsert(data) {
+  async function upsert(data) {
     const authData = {
       id: data.id,
     };
@@ -26,7 +28,7 @@ module.exports = function (injectedStore) {
       authData.username = data.username;
     }
     if (data.password) {
-      authData.password = data.password;
+      authData.password = await bcrypt.hash(data.password, 5);
     }
     return store.upsert(TABLA, authData);
   }
