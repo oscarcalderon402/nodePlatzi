cf07052c76dd4e5e151d650929b590686f45ef2f
Oscar Calderon <gregoriocalderon.07@hotmail.es>
2021-04-20 23:37:03 -0400

    conected microservice

--- mysql/index.js
+++ mysql/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 
 const config = require('../config');
+const router = require('./network');
 
 const app = express();
 
 app.use(express.json());
 
 //rutas
+app.use('/', router);
 
 app.listen(config.mysqlService.port, () => {
   console.log(
