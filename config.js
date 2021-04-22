2a5492e91390a101d7c36807a0e2b8cf8a005836
Oscar Calderon <gregoriocalderon.07@hotmail.es>
2021-04-22 02:05:53 -0400

    conected with our microservice

--- config.js
+++ config.js
@@ -13,6 +13,7 @@ module.exports = {
     database: process.env.MYSQL_DB || 'vLmEwdTFOj',
   },
   mysqlService: {
+    host: process.env.MYSQL_SRV_HOST || 'localhost',
     port: process.env.MYSQL_SRV_PORT || '3001',
   },
 };
